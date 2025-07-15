2757dd64252514d8ec401836331d95c97bf94f5b
Dmitriy <99423721+manea228@users.noreply.github.com>
2025-07-16 02:38:24 +0300

    123

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const { static, Router } = require("express");
-const api = Router();
-const fs = require("fs");
-const { join } = require("path");
-
-
-api.use("/", require("./routes"));
-
-module.exports = api;
--- api/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const api = express.Router();
-const { Collection } = require("discord.js");
-const { join } = require("path");
-const fs = require("fs");
-
-api.use(express.json());
-
-module.exports = api;
